a981f6371b4702ffeeeb26094dc323b10a797423
dylanquaale <dylanquaale33@gmail.com>
2023-02-04 09:16:58 -0600

    TypeError: app.use() requires a middleware function is what I am dealing with right now

--- Main/controllers/home-routes.js
+++ Main/controllers/home-routes.js
@@ -1,8 +1,8 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
+// const router = require('express').Router();
+// const sequelize = require('../config/connection');
 
 
 
 
-module.exports = router;
+// module.exports = router;
 
