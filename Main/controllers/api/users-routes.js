a344b53706326a0565bddd698ef85149f5c28bcd
dylanquaale <dylanquaale33@gmail.com>
2023-02-06 13:39:22 -0600

    Sign up works!

--- Main/controllers/api/users-routes.js
+++ Main/controllers/api/users-routes.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 const { User, } = require('../../models');
 
-router.post('/', async (req, res) => {
-    try {
-      const userData = await User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password:req.body.password,
-      });
+
+    router.post('/', async (req, res) => {
+      try {
+        const userData = await User.create({
+          username: req.body.username,
+          email: req.body.email,
+          password:req.body.password,
+        });
       req.session.save(() => {
         req.session.loggedIn = true;
         res.status(200).json(userData);
