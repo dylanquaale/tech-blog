cb8aa4e283215947c5fc8118a76993a8643fec03
dylanquaale <dylanquaale33@gmail.com>
2023-02-08 10:11:15 -0600

    deleted package.json smh 3001 error

--- Main/controllers/api/index.js
+++ Main/controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./users-routes.js');
-const postRoutes = require('./post-routes.js');
+// const postRoutes = require('./post-routes.js');
 
 
 
 router.use('/users', userRoutes);
-router.use('/post', postRoutes)
+// router.use('/post', postRoutes)
 
 module.exports = router;
--- Main/controllers/home-routes.js
+++ Main/controllers/home-routes.js
@@ -1,50 +1,6 @@
 const router = require("express").Router();
 // const sequelize = require('../config/connection')
-const { Post, User, } = require('../models');
-const withAuth = require('../utils/auth');
-// //23-Ins_Auth Controllers where I took this fro
-// 
-router.get('/', async (req, res) => {
-	try {
-		const postData = await Post.findAll({
-			include: [{
-				model: User,
-				attributes: ['username'],
-			},],
-		});
-
-    const posts = postData.map((post) => post.get({
-			plain: true
-		}));
-
-    res.render('homepage', {posts,loggedIn: req.session.loggedIn});
-  
-  } catch (err) {
-		res.status(500).json(err);
-	}
-});
-
-router.get('/post/:id', async (req, res) => {
-	try {
-		const postData = await Post.findByPk(req.params.id, {
-			include: [
-				{
-					model: User,
-					attributes: ['username'],
-				}, {
-					model: Comment,
-					include: [
-						User
-					]
-				}
-			],
-		});
-
-
-
-
-
-
+// const { Post, User, } = require('../models');
 
 
 router.get("/", async (req, res) => {
