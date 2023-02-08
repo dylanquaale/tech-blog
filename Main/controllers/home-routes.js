19475f910f9cd89b6777509a48255945d070e3dd
dylanquaale <dylanquaale33@gmail.com>
2023-02-08 12:21:42 -0600

    dashboard can create a new post

--- Main/controllers/home-routes.js
+++ Main/controllers/home-routes.js
@@ -1,7 +1,57 @@
 const router = require("express").Router();
 // const sequelize = require('../config/connection')
-// const { Post, User, } = require('../models');
+const { Post, User, Comment } = require('../models');
 
+router.get('/', async (req, res) => {
+	try {
+		const postData = await Post.findAll({
+			include: [{
+				model: User,
+				attributes: ['username'],
+			},],
+		});
+
+		const posts = postData.map((post) => post.get({
+			plain: true
+		}));
+
+		res.render('homepage', {
+			posts,
+			loggedIn: req.session.loggedIn
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+router.get('/post/:id', async (req, res) => {
+	try {
+		const postData = await Post.findByPk(req.params.id, {
+			include: [
+				{
+					model: User,
+					attributes: ['username'],
+				}, {
+					model: Comment,
+					include: [
+						User
+					]
+				}
+			],
+		});
+
+    const post = postData.get({
+			plain: true
+		});
+
+		res.render('blog', {
+			...post,
+			loggedIn: req.session.loggedIn
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
 
 router.get("/", async (req, res) => {
   res.render("homepage", { loggedIn: req.session.loggedIn }); //using template literal
