ac977e8fd62be5ca06ac1982898c89de7a690e42
dylanquaale <dylanquaale33@gmail.com>
2023-02-08 09:20:11 -0600

    working on posts

--- Main/controllers/api/index.js
+++ Main/controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./users-routes.js');
-const postRoutes = require('/post-routes.js');
+const postRoutes = require('./post-routes.js');
 
 
 
--- Main/controllers/home-routes.js
+++ Main/controllers/home-routes.js
@@ -1,16 +1,58 @@
 const router = require("express").Router();
 // const sequelize = require('../config/connection')
-// const { Post, User, } = require('../models');
+const { Post, User, } = require('../models');
+const withAuth = require('../utils/auth');
+// //23-Ins_Auth Controllers where I took this fro
+// 
+router.get('/', async (req, res) => {
+	try {
+		const postData = await Post.findAll({
+			include: [{
+				model: User,
+				attributes: ['username'],
+			},],
+		});
 
-// //23-Ins_Auth Controllers where I took this from
+    const posts = postData.map((post) => post.get({
+			plain: true
+		}));
+
+    res.render('homepage', {posts,loggedIn: req.session.loggedIn});
+  
+  } catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+router.get('/post/:id', async (req, res) => {
+	try {
+		const postData = await Post.findByPk(req.params.id, {
+			include: [
+				{
+					model: User,
+					attributes: ['username'],
+				}, {
+					model: Comment,
+					include: [
+						User
+					]
+				}
+			],
+		});
+
+
+
+
+
+
 
 
 router.get("/", async (req, res) => {
-  res.render("homepage", { loggedIn: req.session.loggedIn });
+  res.render("homepage", { loggedIn: req.session.loggedIn }); //using template literal
 });
 
 router.get("/dashboard", async (req, res) => {
-  res.render("dashboard", { loggedIn: req.session.loggedIn });
+  res.render("dashboard", { loggedIn: req.session.loggedIn });// template literal
 });
 
 router.get("/login", (req, res) => {
