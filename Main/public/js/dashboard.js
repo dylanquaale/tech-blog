dae079d1bf5298582c2c1faaf752ddf3456297d0
dylanquaale <dylanquaale33@gmail.com>
2023-02-08 18:32:19 -0600

    most recent

--- Main/public/js/dashboard.js
+++ Main/public/js/dashboard.js
@@ -1,28 +1,27 @@
 const newFormHandler = async (event) => {
-	event.preventDefault();
+  event.preventDefault();
 
-	const title = document.querySelector('#post-title').value.trim();
-console.log(title);
-	const description = document.querySelector('#post-desc').value.trim();
-console.log(description);
-	if (title && description) {
-		const response = await fetch(`/api/post`, {
-			method: 'POST',
-			body: JSON.stringify({ title, description }),
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		});
-
-		if (response.ok) {
-			document.location.replace('/dashboard');
-		} else {
-			alert('Failed to create post');
-		}
-	}
+  const title = document.querySelector("#post-title").value.trim();
+  console.log(title);
+  const description = document.querySelector("#post-desc").value.trim();
+  console.log(description);
+  if (title && description) {
+    const response = await fetch(`/api/post/`, {
+      method: "POST",
+      body: JSON.stringify({ title, description }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(response);
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert("Failed to create post");
+    }
+  }
 };
 
 document
-	.querySelector('.new-blog-form')
-	.addEventListener('submit', newFormHandler);
-
+  .querySelector(".new-blog-form")
+  .addEventListener("submit", newFormHandler);
