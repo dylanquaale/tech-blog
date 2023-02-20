72022304bf7434c3144bfae3baf9ebb257268b65
dylanquaale <dylanquaale33@gmail.com>
2023-02-20 10:20:38 -0600

    everything working posts going to page

--- Main/utils/helpers.js
+++ Main/utils/helpers.js
@@ -1,13 +1,3 @@
-// function formatDate(date) {
-//     return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
-//         date
-//     ).getFullYear()}`;
-// }
-
-
-// module.exports = {
-//     formatDate
-// }
 
 module.exports = {
     format_date: (date) => {
