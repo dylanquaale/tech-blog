140583dcef40d51832afd9f9538de0d1e25cf055
dylanquaale <dylanquaale33@gmail.com>
2023-02-21 13:39:06 -0600

    Working on comments

--- Main/public/js/comment.js
+++ Main/public/js/comment.js
@@ -1,1 +1,24 @@
+const commentFormHandler = async (event) => {
+    event.preventDefault();
 
+    const post_id = document.querySelector('.new-comment-form').dataset.postid;
+    const comment_text = document.querySelector('#comment_text').value.trim();
+    
+    if (comment_text) {
+		await fetch('/api/comments', {
+			method: 'POST',
+			body: JSON.stringify({
+				post_id,
+				comment_text,
+			}),
+            headers: {
+				'Content-Type': 'application/json'
+			}
+        });
+		document.location.reload();
+	}
+};
+
+document
+	.querySelector('.new-comment-form')
+	.addEventListener('submit', commentFormHandler);
