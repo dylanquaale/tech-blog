9aba7b355c5f89b058e9c57df0953fdb86ab534f
dylanquaale <dylanquaale33@gmail.com>
2023-02-23 18:27:07 -0600

    index changed back

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Post } = require("../../models/potato");
+const { Post } = require("../../models");
 const withAuth = require("../../utils/auth");
 // fix  this
 router.post("/", withAuth, async (req, res) => {
